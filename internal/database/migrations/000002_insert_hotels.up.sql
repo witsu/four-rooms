2f901010b49783d489a7075873dcd8f186d0f9eb
Grzegorz Witos <grzegorz.witos@gmail.com>
2024-12-15 11:10:39 +0200

    Insert hotels

---
+++ internal/database/migrations/000002_insert_hotels.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO hotels (name, address, location) VALUES 
+('Mon Signor', '', 'Los Angeles'),
+('Plaza', 'Fifth Avenue', 'New York'),
+('Continental', '', 'New York'),
+('Bellagio', '', 'Las Vegas');
